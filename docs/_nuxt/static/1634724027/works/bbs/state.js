cad9657ab90ef2eec63fd3c6300b55252e772f5a
kamichanr <kamichanr@gmail.com>
2021-10-20 19:06:00 +0900

    chore: first commit

--- docs/_nuxt/static/1634724027/works/bbs/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634724027",layout:"default",error:a,serverRendered:true,routePath:"\u002Fworks\u002Fbbs",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9f1c5fc8"}}}}(null));
