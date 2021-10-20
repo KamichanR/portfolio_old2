a746ac47c9adb0b863b485821e1459228ceda22f
kamichanr <kamichanr@gmail.com>
2021-10-20 19:31:25 +0900

    chore: first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,6 +72,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extend (config, ctx) {
+      config.output.publicPath = '/portfolio/'
+    }
   },
 
   target: 'static',
