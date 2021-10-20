7d083e06da6921797579f3283896850ddedbfa40
kamichanr <kamichanr@gmail.com>
2021-10-20 19:15:36 +0900

    chore: first commit

--- docs/_nuxt/static/1634724334/works/todo_management_app/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1634724334",layout:"default",error:a,serverRendered:true,routePath:"\u002Fworks\u002Ftodo_management_app",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2b32b6a3"}}}}(null));
